75a6214ea29c42fb0f7a186edec40efa938fe359
sudharshanreddy007 <31057445+sudharshanreddy007@users.noreply.github.com>
2017-10-07 13:27:51 +0530

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,21 @@
+package integerArray;
+
+public class Exception {
+
+	public static void main(String[] args)
+	 
+			{
+		// TODO Auto-generated method stub
+		int a[]=new int[5];//creating an array with size of index 5
+		//declaring and initializing array
+		a[0]=11;
+		a[1]=7;
+		a[2]=71;
+		a[3]=23;
+		a[4]=45;
+		System.out.println(a[5]=35);
+		//here we are trying to print element 6 but we can't.... we get 
+		//ArrayOutOfBoundException
+	}
+
+}
